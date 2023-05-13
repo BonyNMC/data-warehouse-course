bdb79784911937fda88051fa00127fbacc10d9a9
Bony <manhcuongtqt2016@gmail.com>
2023-05-13 15:21:28 +0700

    join customer_category

--- models/analytics/dim_customer.sql
+++ models/analytics/dim_customer.sql
@@ -7,7 +7,9 @@ FROM `vit-lam-data.wide_world_importers.sales__customers`
 , dim_customer__rename_column AS (
 SELECT 
   customer_id AS customer_key
-  , customer_name 
+  , customer_name
+  , customer_category_id AS customer_category_key
+  , buying_group_id AS buying_group_key 
 FROM dim_customer__source
 )
 
@@ -15,10 +17,17 @@ FROM dim_customer__source
 SELECT 
   CAST(customer_key AS INTEGER) AS customer_key
   , CAST(customer_name AS STRING) AS customer_name
+  , CAST(customer_category_key AS INTEGER) AS customer_category_key
+  , CAST(buying_group_key AS INTEGER) AS buying_group_key
 FROM dim_customer__rename_column
 )
 
 SELECT
-  customer_key
-  , customer_name
-FROM dim_customer__cast_type
+  dim_customer.customer_key
+  , dim_customer.customer_name
+  , dim_customer.customer_category_key
+  , dim_customer_category.customer_category_name
+  , dim_customer.buying_group_key
+FROM dim_customer__cast_type AS dim_customer 
+LEFT JOIN {{ ref('stg_dim_customer_category') }} AS dim_customer_category 
+  ON dim_customer.customer_category_key = dim_customer_category.customer_category_key
