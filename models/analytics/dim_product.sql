03d94027ef77c672b3a0cd36bad699ac47794c26
Bony <manhcuongtqt2016@gmail.com>
2023-05-13 16:35:06 +0700

    add is chiller stock

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ SELECT
   stock_item_id AS product_key
   , stock_item_name AS product_name
   , brand AS brand_name
+  , is_chiller_stock AS is_chiller_stock
   , supplier_id AS supplier_key
 FROM dim_product__source
 )
@@ -18,6 +19,7 @@ SELECT
   CAST(product_key AS INTEGER) AS product_key
   , CAST(product_name AS STRING) AS product_name
   , CAST(brand_name AS STRING) AS brand_name
+  , CAST(is_chiller_stock AS BOOLEAN) AS is_chiller_stock
   , CAST(supplier_key AS INTEGER) AS supplier_key
 FROM dim_product__rename_column
 )
@@ -26,6 +28,7 @@ SELECT
   , dim_product.supplier_key
   , dim_product.product_name
   , dim_product.brand_name
+  , dim_product.is_chiller_stock
   , dim_supplier.supplier_name
 FROM dim_product__cast_type AS dim_product
 LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
